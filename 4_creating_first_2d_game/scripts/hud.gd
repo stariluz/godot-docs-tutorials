aa6c726f71545ab24716a09c657a1172690a8561
Adora <im.stariluz@gmail.com>
2026-03-08 13:58:53 -0600

    Add background shader and button animation

--- 4_creating_first_2d_game/scripts/hud.gd
+++ 4_creating_first_2d_game/scripts/hud.gd
@@ -6,16 +6,6 @@ signal start_game
 @export var home_message="Dodge the Creeps!"
 @export var lose_message="Game Over"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func show_message_and_wait(text: String, timer: Timer=$MessageTimer):
 	if timer:
 		timer.start()
